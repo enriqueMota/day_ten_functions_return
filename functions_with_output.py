87fbd5340497d78d03db4cd7f80061f5537a75e8
enriqueMota <abelmota@live.com>
2023-05-23 23:30:29 -0400

    day ten lesson one

---
+++ functions_with_output.py
@@ -0,0 +1,11 @@
+# Functions with outputs
+
+
+# Creating a function that transforms a name into PascalCase
+def format_name(f_name: str, l_name: str):
+    # returning the capitalized names
+    return f"{f_name.capitalize()} {l_name.capitalize()}"
+
+
+
+print(format_name('eNrIqUE', 'mota'))
